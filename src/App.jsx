013e10262a42d730f1667c97b5a17f895a719dd4
AvetisDN <avetisdn@gmail.com>
2021-12-09 14:32:46 +0300

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button, CssBaseline } from "@mui/material";
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <CssBaseline/>
+      <div>
+        <h1>Hello World</h1>
+        <Button variant='contained'>Button</Button>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
